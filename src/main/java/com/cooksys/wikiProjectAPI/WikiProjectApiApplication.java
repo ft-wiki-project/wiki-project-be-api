99e71c521f29425b979f06a9ad8da5f382e475eb
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-24 15:55:17 -0600

    Initial commit

---
+++ src/main/java/com/cooksys/wikiProjectAPI/WikiProjectApiApplication.java
@@ -0,0 +1,13 @@
+package com.cooksys.wikiProjectAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikiProjectApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikiProjectApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cooksys/wikiProjectAPI/WikiProjectApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cooksys.wikiProjectAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikiProjectApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
