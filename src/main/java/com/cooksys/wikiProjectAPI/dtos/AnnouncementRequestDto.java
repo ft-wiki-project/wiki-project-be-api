2c179991b5bf2c2228cf172c0c17f7039b21ee1c
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-26 21:37:34 -0600

    Add AnnouncementRequestDto class with necessary fields and annotations

--- src/main/java/com/cooksys/wikiProjectAPI/dtos/AnnouncementRequestDto.java
+++ src/main/java/com/cooksys/wikiProjectAPI/dtos/AnnouncementRequestDto.java
@@ -1,5 +1,12 @@
 package com.cooksys.wikiProjectAPI.dtos;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 public class AnnouncementRequestDto {
-
+  private String title;
+  private String message;
+  private CompanyRequestDto company;
+  private UserRequestDto author;
 }
