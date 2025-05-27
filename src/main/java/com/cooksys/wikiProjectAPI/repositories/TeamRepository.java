4e6f1ed53492a614a37a31e14ae7a62ee46eca76
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-26 22:38:50 -0600

    Add TeamRepository interface extending JpaRepository for Team entity

--- src/main/java/com/cooksys/wikiProjectAPI/repositories/TeamRepository.java
+++ src/main/java/com/cooksys/wikiProjectAPI/repositories/TeamRepository.java
@@ -1,5 +1,9 @@
 package com.cooksys.wikiProjectAPI.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.cooksys.wikiProjectAPI.entities.Team;
 
-public interface TeamRepository {
+@Repository
+public interface TeamRepository extends JpaRepository<Team, Long> {
 
 }
