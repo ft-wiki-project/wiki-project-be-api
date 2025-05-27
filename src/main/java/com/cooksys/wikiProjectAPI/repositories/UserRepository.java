e0d4e1542476b61eb966283068bf54ec0c56a064
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-26 22:38:45 -0600

    Add UserRepository interface extending JpaRepository for User entity

--- src/main/java/com/cooksys/wikiProjectAPI/repositories/UserRepository.java
+++ src/main/java/com/cooksys/wikiProjectAPI/repositories/UserRepository.java
@@ -1,5 +1,9 @@
 package com.cooksys.wikiProjectAPI.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.cooksys.wikiProjectAPI.entities.User;
 
-public interface UserRepository {
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
 
 }
