bfa0ad617e728c46b1a3196bfa41e48a607e3ebe
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-27 00:43:49 -0600

    Add method to UserRepository for finding user by credentials

--- src/main/java/com/cooksys/wikiProjectAPI/repositories/UserRepository.java
+++ src/main/java/com/cooksys/wikiProjectAPI/repositories/UserRepository.java
@@ -1,9 +1,13 @@
 package com.cooksys.wikiProjectAPI.repositories;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.cooksys.wikiProjectAPI.entities.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+  Optional<User> findByCredentialsUsernameAndCredentialsPassword(String username, String password);
 
 }
