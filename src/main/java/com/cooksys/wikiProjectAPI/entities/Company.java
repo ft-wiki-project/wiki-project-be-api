03f3fff50c7c0bcc03eb335ffdc64d7c4de90557
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-26 19:34:06 -0600

    Add Company entity with fields for id, name, and description

---
+++ src/main/java/com/cooksys/wikiProjectAPI/entities/Company.java
@@ -0,0 +1,19 @@
+package com.cooksys.wikiProjectAPI.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Company {
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  private String name;
+  private String description;
+}
