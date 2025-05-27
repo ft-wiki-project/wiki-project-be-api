b270a674565d0c534f36124751ae49c4cd1fb147
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-26 19:24:52 -0600

    Add Team entity with fields for id, name, and description

---
+++ src/main/java/com/cooksys/wikiProjectAPI/entities/Team.java
@@ -0,0 +1,21 @@
+package com.cooksys.wikiProjectAPI.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Team {
+  @Id
+  @GeneratedValue
+  private Long id;
+
+  private String name;
+  private String description;
+}
