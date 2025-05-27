be021e6bb023de3f49823326812d18fdf881af71
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-26 22:38:57 -0600

    Add ProjectRepository interface extending JpaRepository for Project entity

--- src/main/java/com/cooksys/wikiProjectAPI/repositories/ProjectRepository.java
+++ src/main/java/com/cooksys/wikiProjectAPI/repositories/ProjectRepository.java
@@ -1,5 +1,9 @@
 package com.cooksys.wikiProjectAPI.repositories;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.cooksys.wikiProjectAPI.entities.Project;
 
-public interface ProjectRepository {
+@Repository
+public interface ProjectRepository extends JpaRepository<Project, Long> {
 
 }
