78c4aeadde2b671880048a81494d441ea93fe396
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-26 20:55:58 -0600

    Add ProfileDto class with validation annotations for email and fields for first and last name

--- src/main/java/com/cooksys/wikiProjectAPI/dtos/ProfileDto.java
+++ src/main/java/com/cooksys/wikiProjectAPI/dtos/ProfileDto.java
@@ -1,5 +1,18 @@
 package com.cooksys.wikiProjectAPI.dtos;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@Data
 public class ProfileDto {
+  private String firstName;
+  private String lastName;
 
+  @NotNull(message = "Email cannot be null")
+  @Email(message = "Please enter a valid email address")
+  private String email;
+
+  private String phone;
 }
