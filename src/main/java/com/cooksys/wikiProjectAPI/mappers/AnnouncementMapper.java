fecc33ad5e6dab2a2f87bffc40635abec92f6181
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-26 22:19:40 -0600

    Add AnnouncementMapper interface for mapping AnnouncementRequestDto and AnnouncementResponseDto

--- src/main/java/com/cooksys/wikiProjectAPI/mappers/AnnouncementMapper.java
+++ src/main/java/com/cooksys/wikiProjectAPI/mappers/AnnouncementMapper.java
@@ -1,5 +1,19 @@
 package com.cooksys.wikiProjectAPI.mappers;
+import java.util.List;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
+import com.cooksys.wikiProjectAPI.dtos.AnnouncementRequestDto;
+import com.cooksys.wikiProjectAPI.dtos.AnnouncementResponseDto;
+import com.cooksys.wikiProjectAPI.entities.Announcement;
+
+@Mapper(componentModel = "spring")
 public interface AnnouncementMapper {
-
+  Announcement requestDtoToEntity(AnnouncementRequestDto announcementRequestDto);
+  
+  @Mapping(target = "author.username", source = "author.credentials.username")
+  List<AnnouncementResponseDto> entitiesToDtos(List<Announcement> announcements);
+  
+  @Mapping(target = "author.username", source = "author.credentials.username")
+  AnnouncementResponseDto entityToDto(Announcement announcement);
 }
