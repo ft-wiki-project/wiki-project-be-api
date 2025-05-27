2f74ced9aaaf5214b34c853a6d3373a0b4b58e43
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-26 20:44:00 -0600

    Add ManyToOne relationship between Project and Team entities

--- src/main/java/com/cooksys/wikiProjectAPI/entities/Project.java
+++ src/main/java/com/cooksys/wikiProjectAPI/entities/Project.java
@@ -3,6 +3,8 @@ package com.cooksys.wikiProjectAPI.entities;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -19,4 +21,8 @@ public class Project {
   private String name;
   private String description;
   private boolean active = true;
+
+  @ManyToOne
+  @JoinColumn(name = "team_id")
+  private Team team;
 }
