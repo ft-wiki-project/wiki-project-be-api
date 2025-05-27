485d6fc1ccf2ac20a7598748553b54ee4a01df03
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-26 21:36:39 -0600

    Add UserRequestDto class with necessary fields and annotations

--- src/main/java/com/cooksys/wikiProjectAPI/dtos/UserRequestDto.java
+++ src/main/java/com/cooksys/wikiProjectAPI/dtos/UserRequestDto.java
@@ -1,5 +1,13 @@
 package com.cooksys.wikiProjectAPI.dtos;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 public class UserRequestDto {
-
+  private CredentialsDto credentials;
+  private ProfileDto profile;
+  private String status;
+  private boolean admin;
+  private boolean active;
 }
