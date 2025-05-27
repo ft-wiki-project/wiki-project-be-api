a9eb17e4a5d15935ff185327d334105ca967cefb
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-26 22:19:10 -0600

    Add ProfileMapper interface for mapping ProfileDto and Profile entities

--- src/main/java/com/cooksys/wikiProjectAPI/mappers/ProfileMapper.java
+++ src/main/java/com/cooksys/wikiProjectAPI/mappers/ProfileMapper.java
@@ -1,5 +1,11 @@
 package com.cooksys.wikiProjectAPI.mappers;
+import org.mapstruct.Mapper;
+import com.cooksys.wikiProjectAPI.dtos.ProfileDto;
+import com.cooksys.wikiProjectAPI.embeddables.Profile;
 
+@Mapper(componentModel = "spring")
 public interface ProfileMapper {
-
+  Profile profileDtoToEntity(ProfileDto profileDto);
+  
+  ProfileDto profileEntityToDto(Profile profile);
 }
