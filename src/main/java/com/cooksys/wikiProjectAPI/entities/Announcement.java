c881b196823a30ccae9f8421bc551bbdc20c1f2f
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-26 20:39:41 -0600

    Add ManyToOne relationships for author and company in Announcement entity

--- src/main/java/com/cooksys/wikiProjectAPI/entities/Announcement.java
+++ src/main/java/com/cooksys/wikiProjectAPI/entities/Announcement.java
@@ -7,6 +7,8 @@ import org.hibernate.annotations.CreationTimestamp;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -26,4 +28,12 @@ public class Announcement {
   private String title;
   
   private String message;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private User author;
+
+  @ManyToOne
+  @JoinColumn(name = "company_id")
+  private Company company;
 }
