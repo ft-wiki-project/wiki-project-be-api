bfe2cb5cf61c60165f0bc68da2ecfc51ac292ca8
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-26 22:19:31 -0600

    Add CompanyMapper interface for mapping CompanyRequestDto and CompanyResponseDto

--- src/main/java/com/cooksys/wikiProjectAPI/mappers/CompanyMapper.java
+++ src/main/java/com/cooksys/wikiProjectAPI/mappers/CompanyMapper.java
@@ -1,5 +1,18 @@
 package com.cooksys.wikiProjectAPI.mappers;
 
-public interface CompanyMapper {
+import java.util.List;
 
+import org.mapstruct.Mapper;
+
+import com.cooksys.wikiProjectAPI.dtos.CompanyRequestDto;
+import com.cooksys.wikiProjectAPI.dtos.CompanyResponseDto;
+import com.cooksys.wikiProjectAPI.entities.Company;
+
+@Mapper(componentModel = "spring")
+public interface CompanyMapper {
+  Company requestDtoToEntity(CompanyRequestDto companyRequestDto);
+
+  CompanyResponseDto entityToDto(Company company);
+  
+  List<CompanyResponseDto> entitiesToDtos(List<Company> companies);
 }
