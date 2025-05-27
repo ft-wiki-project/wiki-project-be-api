4a1b2c6866bbb94f56ae5b1cbe69ec91e50d020b
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-27 01:24:38 -0600

    Add method signature for retrieving announcements by company ID in AnnouncementService

--- src/main/java/com/cooksys/wikiProjectAPI/services/AnnouncementService.java
+++ src/main/java/com/cooksys/wikiProjectAPI/services/AnnouncementService.java
@@ -1,5 +1,11 @@
 package com.cooksys.wikiProjectAPI.services;
 
+import java.util.List;
+
+import com.cooksys.wikiProjectAPI.dtos.AnnouncementResponseDto;
+
 public interface AnnouncementService {
+
+  List<AnnouncementResponseDto> getAnnouncementsByCompanyId(Long companyId);
 
 }
