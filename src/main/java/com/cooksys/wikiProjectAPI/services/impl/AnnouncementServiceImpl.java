314395ef7c143d5731cb6cd7900ef68449b2bb81
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-26 23:02:39 -0600

    Add Spring annotations and implement AnnouncementService interface in AnnouncementServiceImpl

--- src/main/java/com/cooksys/wikiProjectAPI/services/impl/AnnouncementServiceImpl.java
+++ src/main/java/com/cooksys/wikiProjectAPI/services/impl/AnnouncementServiceImpl.java
@@ -1,5 +1,10 @@
 package com.cooksys.wikiProjectAPI.services.impl;
+import org.springframework.stereotype.Service;
+import com.cooksys.wikiProjectAPI.services.AnnouncementService;
+import lombok.RequiredArgsConstructor;
 
-public class AnnouncementServiceImpl {
+@Service
+@RequiredArgsConstructor
+public class AnnouncementServiceImpl implements AnnouncementService {
 
 }
