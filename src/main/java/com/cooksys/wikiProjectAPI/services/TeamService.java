b86b3ee2cfbf6cb048636c915a7f7c75f0b86ba1
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-27 17:17:38 -0600

    Add createTeam method to TeamService interface

--- src/main/java/com/cooksys/wikiProjectAPI/services/TeamService.java
+++ src/main/java/com/cooksys/wikiProjectAPI/services/TeamService.java
@@ -1,5 +1,10 @@
 package com.cooksys.wikiProjectAPI.services;
 
+import com.cooksys.wikiProjectAPI.dtos.TeamRequestDto;
+import com.cooksys.wikiProjectAPI.dtos.TeamResponseDto;
+
 public interface TeamService {
+
+  TeamResponseDto createTeam(TeamRequestDto teamRequestDto);
 
 }
