2d845e21d0ee1782c9babef8e5f980aaf210e8bc
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-26 21:37:38 -0600

    Add CompanyRequestDto class with Lombok annotations for data handling

--- src/main/java/com/cooksys/wikiProjectAPI/dtos/CompanyRequestDto.java
+++ src/main/java/com/cooksys/wikiProjectAPI/dtos/CompanyRequestDto.java
@@ -1,5 +1,11 @@
 package com.cooksys.wikiProjectAPI.dtos;
 
-public class CompanyRequestDto {
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
+public class CompanyRequestDto {
+  private String name;
+  private String description;
 }
