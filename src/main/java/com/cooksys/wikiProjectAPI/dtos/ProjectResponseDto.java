edd330950ad186ee3172b44e385837c1ab7b65ed
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-26 21:37:05 -0600

    Add ProjectResponseDto class with necessary fields and annotations

--- src/main/java/com/cooksys/wikiProjectAPI/dtos/ProjectResponseDto.java
+++ src/main/java/com/cooksys/wikiProjectAPI/dtos/ProjectResponseDto.java
@@ -1,5 +1,13 @@
 package com.cooksys.wikiProjectAPI.dtos;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@NoArgsConstructor
 public class ProjectResponseDto {
-
+  private Long id;
+  private String name;
+  private String description;
+  private boolean active;
+  private TeamResponseDto team;
 }
