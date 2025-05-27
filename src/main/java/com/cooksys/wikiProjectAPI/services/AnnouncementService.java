3e42cf7fb1fb4c94fe1794d9553730b4eb843b86
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-27 10:26:04 -0600

    Add createAnnouncement method to AnnouncementService interface

--- src/main/java/com/cooksys/wikiProjectAPI/services/AnnouncementService.java
+++ src/main/java/com/cooksys/wikiProjectAPI/services/AnnouncementService.java
@@ -2,10 +2,13 @@ package com.cooksys.wikiProjectAPI.services;
 
 import java.util.List;
 
+import com.cooksys.wikiProjectAPI.dtos.AnnouncementRequestDto;
 import com.cooksys.wikiProjectAPI.dtos.AnnouncementResponseDto;
 
 public interface AnnouncementService {
 
   List<AnnouncementResponseDto> getAnnouncementsByCompanyId(Long companyId);
+
+  AnnouncementResponseDto createAnnouncement(AnnouncementRequestDto announcementRequestDto);
 
 }
