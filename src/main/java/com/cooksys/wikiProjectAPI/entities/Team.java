aa988b38f56b596ab4cc64f8f41301f0ab4007a8
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-26 20:44:05 -0600

    Add ManyToOne and OneToMany relationships in Team entity

--- src/main/java/com/cooksys/wikiProjectAPI/entities/Team.java
+++ src/main/java/com/cooksys/wikiProjectAPI/entities/Team.java
@@ -6,7 +6,10 @@ import java.util.List;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -25,4 +28,11 @@ public class Team {
 
   @ManyToMany(mappedBy = "teams")
   private List<User> users = new ArrayList<>();
+
+  @ManyToOne
+  @JoinColumn(name = "company_id")
+  private Company company;
+
+  @OneToMany(mappedBy = "team")
+  private List<Project> projects = new ArrayList<>();
 }
