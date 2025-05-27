48d6f7e5d98946c0bc3eb8a58480bc1c0d054add
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-26 21:37:09 -0600

    Add ProjectRequestDto class with necessary fields and annotations

--- src/main/java/com/cooksys/wikiProjectAPI/dtos/ProjectRequestDto.java
+++ src/main/java/com/cooksys/wikiProjectAPI/dtos/ProjectRequestDto.java
@@ -1,5 +1,15 @@
 package com.cooksys.wikiProjectAPI.dtos;
 
-public class ProjectRequestDto {
+import com.cooksys.wikiProjectAPI.entities.Team;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ProjectRequestDto {
+  private String name;
+  private String description;
+  private boolean active;
+  private Team team;
 }
