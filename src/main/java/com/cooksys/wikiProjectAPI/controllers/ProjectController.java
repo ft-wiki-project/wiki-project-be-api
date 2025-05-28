76d6eef9a0c8584ac2e6e7bbf41f2af6b72eacdf
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-27 18:10:09 -0600

    Implement createProject endpoint in ProjectController

--- src/main/java/com/cooksys/wikiProjectAPI/controllers/ProjectController.java
+++ src/main/java/com/cooksys/wikiProjectAPI/controllers/ProjectController.java
@@ -1,11 +1,25 @@
 package com.cooksys.wikiProjectAPI.controllers;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cooksys.wikiProjectAPI.dtos.ProjectRequestDto;
+import com.cooksys.wikiProjectAPI.dtos.ProjectResponseDto;
+import com.cooksys.wikiProjectAPI.services.ProjectService;
+
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/projects")
 @RequiredArgsConstructor
 public class ProjectController {
+
+  private final ProjectService projectService;
+  
+  @PostMapping()
+  public ProjectResponseDto createProject(@RequestBody ProjectRequestDto projectRequestDto) {
+    return projectService.createProject(projectRequestDto);
+  }
 
 }
