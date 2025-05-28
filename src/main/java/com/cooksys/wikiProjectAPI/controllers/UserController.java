96629b5b0d8ff7dbab13049c10018cee0815d9de
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-27 21:20:12 -0600

    Change login endpoint to use POST method

--- src/main/java/com/cooksys/wikiProjectAPI/controllers/UserController.java
+++ src/main/java/com/cooksys/wikiProjectAPI/controllers/UserController.java
@@ -1,13 +1,9 @@
 package com.cooksys.wikiProjectAPI.controllers;
-import java.util.List;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cooksys.wikiProjectAPI.dtos.CompanyResponseDto;
 import com.cooksys.wikiProjectAPI.dtos.CredentialsDto;
 import com.cooksys.wikiProjectAPI.dtos.UserResponseDto;
 import com.cooksys.wikiProjectAPI.services.UserService;
@@ -20,11 +16,8 @@ import lombok.RequiredArgsConstructor;
 public class UserController {
   private final UserService userService;
 
-  @GetMapping("/login")
+  @PostMapping("/login")
   public UserResponseDto login(@RequestBody CredentialsDto credentialsDto) {
     return userService.login(credentialsDto);
   }
-  
-  
-  
 }
