9497fbca90dc27c5405de9fba3ef0aaf7f2d867a
AlecKap <120225785+AlecKap@users.noreply.github.com>
2025-05-27 19:03:33 -0600

    Add getProjectsByTeamId method to ProjectService interface

--- src/main/java/com/cooksys/wikiProjectAPI/services/ProjectService.java
+++ src/main/java/com/cooksys/wikiProjectAPI/services/ProjectService.java
@@ -1,10 +1,14 @@
 package com.cooksys.wikiProjectAPI.services;
 
+import java.util.List;
+
 import com.cooksys.wikiProjectAPI.dtos.ProjectRequestDto;
 import com.cooksys.wikiProjectAPI.dtos.ProjectResponseDto;
 
 public interface ProjectService {
 
   ProjectResponseDto createProject(ProjectRequestDto projectRequestDto);
+
+  List<ProjectResponseDto> getProjectsByTeamId(Long teamId);
 
 }
